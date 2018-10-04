ab9fdd4662b9718ff4bf588b9f12ccbc8e6bebf0
Sam Eaton <sameaton1989@gmail.com>
2018-10-03 20:00:54 -0400

    fixed js

--- assets/javascript/images.js
+++ assets/javascript/images.js
@@ -1,9 +1,7 @@
-$(function(){
-
-})
-
+$(document).ready(function() {
+    renderButtons();
+});
 var randomPeople = ["Hakeem Olajuwon", "Rock Hudson", "Marina Abramovic", "Kenneth Anger", "Sid Vicious", "Yayoi Kusama", "Ziggy Stardust", "Stan Brakhage", "DMX", "Manute Bol", "Andrezj Zulawski", "GG Allin", "Rasheed Wallace", "Klaus Kinski", "Busta Rhymes", "Richard Nixon", "Ric Flair", "John Carradine", "Lon Chaney Jr.", "Edward G. Robinson", "Guy Fieri", "Billy Bob Thornton", "Karl Marx", "Gilbert Gottfried", "Agnes Varda", "Udo Kier", "Larry Johnson", "Rico Suave", "Garrincha", "Miles Davis"];
-
 
 function renderButtons() {
     $("#buttons-display").empty();
@@ -23,51 +21,48 @@ $("#add-image").on("click", function(event) {
     randomPeople.push(searchResult);
     console.log(searchResult);
     renderButtons();
-
-})
+});
 
 $(document).on("click", ".person", function() {
     var type = $(this).data("name");
     console.log(type)
     var search = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=xCcrRbcoALUlRySaKH6td4TAqg02ltSd&limit=10"; $.ajax({
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=xCcrRbcoALUlRySaKH6td4TAqg02ltSd&limit=10"; 
+    $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
+    console.log(response)
     var results = response.data;
     for (var i = 0; i < results.length; i++) {
         var gifDiv = $("<div class='returned-image'>");
         var rating = results[i].rating;
-        var p = $("<p>").text("Rating: " + results[i].rating);
-        var animated = response.data[i].images.fixed_height.url;
-        var still = response.data[i].images.fixed_height_still.url;
+        var p = $("<p>").text("Rating: " + rating);
+        var animated = results[i].images.fixed_height.url;
+        var still = results[i].images.fixed_height_still.url;
         var personImage = $("<img>");
         personImage.attr("src", still);
         personImage.attr("data-still", still);
         personImage.attr("data-animated", animated);
-        personImage.attr("data-state", "still")
-        personImage.addClass("searchImage");
+        personImage.attr("data-state", "still");
+        console.log(personImage.attr);
+        personImage.addClass("search-image");
         gifDiv.append(p);
-        gifDiv.append(personImage)
-        $("#image-area").prepend(gifDiv)
-    }
+        gifDiv.append(personImage);
+        $("#image-area").prepend(gifDiv);
+        }
+    });
 });
 
-$(document).on("click", ".searchImage", function() {
+$(document).on("click", ".search-image", function() {
+
     var state = $(this).attr("data-state");
-    if(state == "still") {
+    console.log(state);
+    if(state === "still") {
         $(this).attr("src", $(this).data("animated"));
         $(this).attr("data-state", "animated");
     } else {
         $(this).attr("src", $(this).data("still"));
         $(this).attr("data-state", "still");
-    }
-    
-
-})
-
-
-
+    } 
 });
-
-renderButtons();
