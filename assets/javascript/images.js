66f88511f63b23d68b2454e25d8815b0f50aae66
Sam Eaton <sameaton1989@gmail.com>
2018-10-05 18:21:05 -0400

    took out an image from background variable

--- assets/javascript/images.js
+++ assets/javascript/images.js
@@ -24,7 +24,7 @@ $("#add-image").on("click", function(event) {
 });
 
 $(document).on("click", ".person", function () {
-    var images = ["tv.jpg", "fuzz.jpg", "1.jpg", "6.jpg", "fuzz.png", "tv-fuzz.jpg", "bars.png", "8.jpg", "9.jpg", "10.jpg", "3.jpg", "11.jpg", "12.gif", "13.gif", "14.gif", "15.jpg", "16.gif", "17.gif", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg"];
+    var images = ["tv.jpg", "1.jpg", "6.jpg", "fuzz.png", "tv-fuzz.jpg", "bars.png", "8.jpg", "9.jpg", "10.jpg", "3.jpg", "11.jpg", "12.gif", "13.gif", "14.gif", "15.jpg", "16.gif", "17.gif", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg"];
     $("#image-area").css({"background-image": "url(assets/images/" + images[Math.floor(Math.random() * images.length)] + ")"});
    });
 
